046f425ffa978370a7f2deda70ea437f99e07a17
ChunTungZhuangLeo <czhuang3@jh.edu>
2024-06-11 10:02:55 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import CTScanMesh from './components/CTScanMesh/CTScanMesh.jsx';
 import Footer from './components/Footer/Footer.jsx';
 const App = () => {
     const videos1 = [
-        'https://mrgiovanni.github.io/bodymaps/utils/AbdomenAtlas%201.0/BDMAP_00000020.mp4',
-        'https://mrgiovanni.github.io/bodymaps/utils/AbdomenAtlas%201.0/BDMAP_00000031.mp4',
-        'https://mrgiovanni.github.io/bodymaps/utils/AbdomenAtlas%201.0/BDMAP_00000205.mp4'
-      ];
-      
-      const videos2 = [
-        'https://mrgiovanni.github.io/bodymaps/utils/AbdomenAtlas%201.1/BDMAP_00000006.mp4',
-        'https://mrgiovanni.github.io/bodymaps/utils/AbdomenAtlas%201.1/BDMAP_00000031.mp4'
-      ];
+        'https://raw.githubusercontent.com/MrGiovanni/bodymaps/gh-pages/videos/AbdomenAtlas1.0/BDMAP_00000020.mp4',
+        'https://raw.githubusercontent.com/MrGiovanni/bodymaps/gh-pages/videos/AbdomenAtlas1.0/BDMAP_00000031.mp4',
+        'https://raw.githubusercontent.com/MrGiovanni/bodymaps/gh-pages/videos/AbdomenAtlas1.0/BDMAP_00000205.mp4'
+    ];
 
+    const videos2 = [
+        'https://raw.githubusercontent.com/MrGiovanni/bodymaps/gh-pages/videos/AbdomenAtlas1.1/BDMAP_00000006.mp4',
+        'https://raw.githubusercontent.com/MrGiovanni/bodymaps/gh-pages/videos/AbdomenAtlas1.1/BDMAP_00000031.mp4'
+    ];
+    
     return (
         <div className="main-container">
             {/* Abdomen Atlas Section */}
